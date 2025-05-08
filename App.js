8dfe7521aa4b93271c6b4091d0cec4e67466339a
Emaan-Binte-Ehsan <140392051+Emaan-Binte-Ehsan@users.noreply.github.com>
2025-05-08 08:41:50 -0700

    Made Changes in App.js

--- App.js
+++ App.js
@@ -3,17 +3,22 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SignupScreen from './Screens/SignupScreen';
 import LoginScreen from './Screens/LoginScreen';
 import Profile from './Screens/Profile';
+import SplashScreen from './Screens/SplashScreen';
+import GuideScreen1 from './Screens/GuideScreen1';
 import OnboardingScreen from './Screens/OnboardingScreen';
+
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Signup">
+      <Stack.Navigator initialRouteName="Splash" >
+      <Stack.Screen name="Splash" component={SplashScreen} />
+      <Stack.Screen name="Guide1" component={GuideScreen1} />
         <Stack.Screen name="Signup" component={SignupScreen} />
-        <Stack.Screen name="Onboarding" component={OnboardingScreen} /> 
         <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Onboarding" component={OnboardingScreen} /> 
         <Stack.Screen name="Profile" component={Profile} />
         {/* Add other screens here */}
       </Stack.Navigator>
